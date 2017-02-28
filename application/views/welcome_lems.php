4fd9ccba7365fd48839846e107d591e9e25ecde7
imadx <ahtimadhusanka@gmail.com>
2017-02-28 13:04:39 +0530

    initial commit

---
+++ application/views/welcome_lems.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Welcome to LEMS</title>
+		<link rel="stylesheet" href="/assets/css/semantic.min.css">
+		<script src="/assets/js/jquery-3.1.1.min.js"></script>
+		<script src="/assets/js/semantic.min.js"></script>
+	</head>
+	<body>
+	<br><br><br>
+			<div class="ui piled container segment">
+				<h1 class="ui teal header">
+				Welcome to LEMS
+				<div class="sub header">LEARN Event Management System</div>
+				</h1>
+				<p class="ui orange segment"><i class="info icon"></i>This system is on hold. Sorry about the inconvenience</p>
+			</div>
+	</body>
+</html>
